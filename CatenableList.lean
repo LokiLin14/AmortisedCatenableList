0c49c99367dd78d0735ac96694c1fa22df294d87
ll5323 <ll5323@ic.ac.uk>
2025-05-25 11:34:05 +0100

    some formalisations of proof from PFDS

--- CatenableList.lean
+++ CatenableList.lean
@@ -1,42 +1,60 @@
-inductive CatenableList' where
-  | ccons : List CatenableList' -> CatenableList'
+import Mathlib.Tactic.Ring
+import Mathlib.Tactic.Linarith
+
+inductive CatenableList' (A : Type _) where
+  | ccons : A -> List (CatenableList' A) -> CatenableList' A
 deriving Repr, Hashable, BEq
 
-inductive CatenableList where
-  | nil : CatenableList
-  | catList : CatenableList' -> CatenableList
+inductive CatenableList (A : Type _) where
+  | nil : CatenableList A
+  | catList : CatenableList' A -> CatenableList A
 deriving Repr, Hashable, BEq
 
 namespace CatenableList
 open CatenableList'
 
-def snoc : CatenableList -> CatenableList
-  | nil => catList (ccons [])
-  | catList (ccons cls) => catList (ccons (cls ++ [ccons []]))
+def snoc (cl : CatenableList A) (x : A) : CatenableList A :=
+  match cl with
+  | nil => catList (ccons x [])
+  | catList (ccons a cls) => catList (ccons a (cls ++ [ccons x []]))
 
-def cons : CatenableList -> CatenableList
-  | nil => catList (ccons [])
-  | catList cl' => catList (ccons [cl'])
+def cons (x : A) (cl : CatenableList A) : CatenableList A :=
+  match cl with
+  | nil => catList (ccons x [])
+  | catList cl' => catList (ccons x [cl'])
 
-def append (cl cl' : CatenableList) : CatenableList :=
+def append (cl cl' : CatenableList A) : CatenableList A :=
   match (cl, cl') with
   | (nil, _) => cl'
   | (_, nil) => cl
-  | (catList (ccons ncls), catList ncl') => catList (ccons (ncls ++ [ncl']))
+  | (catList (ccons a ncls), catList ncl') => catList (ccons a (ncls ++ [ncl']))
 
-def tail : CatenableList -> CatenableList
+def tail (cl : CatenableList A) : CatenableList A :=
+  match cl with
   | nil => nil
-  | catList (ccons ncls) => ncls.map catList |> List.foldr append nil
+  | catList (ccons _ ncls) => ncls.map catList |> List.foldr append nil
 
-def potential : CatenableList -> Int
+-- greedy algorithm that calculates n - maximum debits
+def potential (cl :  CatenableList A) : Int :=
+  match cl with
   | nil => 0
   | catList ncl => go 0 ncl
     where
-      go (acc : Int) (ncl : CatenableList') : Int :=
-        let (ccons ncls) := ncl;
+      go (acc : Int) (ncl : CatenableList' A) : Int :=
+        let (ccons _ ncls) := ncl;
         let contrib := if acc >= 0 then min acc ncls.length else 0;
           (ncls.foldl go (acc + 2 - contrib)) - 1
 
+def tailCost : CatenableList A -> Nat
+  | nil => 0
+  | catList (ccons _ ncls) => ncls.length
+
+theorem tailIsConstantTime : ∃ k : Nat, ∀ cl : CatenableList A,
+  tailCost cl ≤ k - (potential (tail cl) - potential cl) := by
+  exists 3
+  sorry
+
+-- Checking potential function is valid by applying a random sequence of operations
 inductive AmortisationOp where
   | consOp : AmortisationOp
   | snocOp : AmortisationOp
@@ -44,12 +62,9 @@ inductive AmortisationOp where
 deriving Repr
 open AmortisationOp
 
-theorem potentialStartsAtZero : potential nil = 0 := by
-  rfl
-
 def stressPotential (numOperations : Nat) (seed : Nat := 0) : IO Unit := do
   let maxAmortisedCost := 3
-  let mut clist := nil
+  let mut clist : CatenableList Nat := nil
   let mut rng := mkStdGen seed
   for iteration in [1:numOperations] do
     let (opNum, rng') := stdNext rng
@@ -59,12 +74,12 @@ def stressPotential (numOperations : Nat) (seed : Nat := 0) : IO Unit := do
       | 1 => snocOp
       | _ => tailOp
     let (clist', opCost) := match op with
-      | consOp => (cons clist, 1)
-      | snocOp => (snoc clist, 1)
+      | consOp => (cons 0 clist, 1)
+      | snocOp => (snoc clist 0, 1)
       | tailOp => (tail clist,
         match clist with
         | nil => 1
-        | catList (ccons ncls) => ncls.length)
+        | catList (ccons _ ncls) => ncls.length)
     if !(opCost <= maxAmortisedCost - (potential clist' - potential clist)) then
       println! s!"Amortisation failed when applying {repr op}!!! :/"
       println! s!"Equation does not hold: ({opCost} <= {maxAmortisedCost} - ({potential clist'} - {potential clist}))"
@@ -81,3 +96,26 @@ def stressPotential (numOperations : Nat) (seed : Nat := 0) : IO Unit := do
     clist := clist'
   println! s!"Stress failed to break amortisation :)"
 end CatenableList
+
+structure DebitList' where
+  ccons :: (debits : Nat) (childs : List DebitList')
+
+def DebitList'.distanceFromIndex (acc : Nat) (dl : DebitList') : Nat :=
+  let (DebitList'.ccons debits children) := dl
+  children.foldl distanceFromIndex (acc - dl.debits + 2) - 1
+
+def List.inits' : (l : List α) -> List (List α × α)
+  | List.nil => []
+  | List.cons a as => ([], a) :: (inits' as).map (fun (as', a') => (a :: as', a'))
+-- #eval List.inits' [1, 2, 3] -- [([], 1), ([1], 2), ([1, 2], 3)]
+
+inductive ValidDebitList' : (acc : Nat) → (dl : DebitList') → Type where
+  | proofs {acc : Nat} {dl : DebitList'}
+    (debitsLeAcc : (dl.debits ≤ acc))
+    (validChilds : ∀ p ∈ (List.inits' dl.childs),
+      ValidDebitList' (p.fst.foldl DebitList'.distanceFromIndex (acc - dl.debits + 2)) p.snd) :
+    ValidDebitList' acc dl
+
+inductive DebitList where
+  | nil : DebitList
+  | debitList : (dl : DebitList') -> ValidDebitList' 0 dl -> DebitList
--- Main.lean
+++ Main.lean
@@ -2,4 +2,4 @@ import CatenableList
 open CatenableList
 
 def main (_ : List String): IO Unit :=
-  stressPotential 1000000
+  stressPotential 100000
